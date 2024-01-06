a04b7f3807005ee257244490b003e82c7040138a
Matt Signorelli <mgs255@cornell.edu>
2024-01-06 14:24:52 -0500

    Fixed type stability for GTPSA benchmark

--- benchmark/taylormap.jl
+++ benchmark/taylormap.jl
@@ -61,11 +61,10 @@ function track_ring(z0, k1, k2l, kick)
 end
 
 function benchmark_GTPSA()
-  m(z) = track_ring([z[1], z[2], z[3], z[4]], 0.36+z[5], z[6], z[7:end])
-  d = Descriptor(4,2,52,2)#,52,2)
+  d = Descriptor(4,2,52,2)
   z = vars(d)
   k = params(d)
-  map = m([z...,k...])
+  map = track_ring([z[1], z[2], z[3], z[4]], 0.36+k[1], k[2], k[3:end])
   j = jacobian(map, include_params=true)
   h1 = hessian(map[1], include_params=true)
   h2 = hessian(map[2], include_params=true)
