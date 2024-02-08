610f293dc4fe8f10d36e82b23fbab5526ec610aa
Matt Signorelli <mgs255@cornell.edu>
2024-02-08 15:25:41 -0500

    fix sextupole

--- benchmark/taylormap.jl
+++ benchmark/taylormap.jl
@@ -60,7 +60,7 @@ function track_drift(z0)
 end
 
 function track_sextupole(z0, k2l)
-  z2 = @FastGTPSA z0[2]-k2l/2.0*(z0[1]^2 - z0[3]^2)
+  z2 = @FastGTPSA z0[2]-k2l*(z0[1]^2 - z0[3]^2)
   z4 = @FastGTPSA z0[4]+k2l/2.0*z0[1]*z0[3]
   return  [z0[1], z2, z0[3], z4]
 end
@@ -83,7 +83,7 @@ function track_ring(z0, k1=0.36, k2l=1.2, kick=zeros(50))
 end
 
 function benchmark_GTPSA()
-  d = Descriptor(4,2,52,2)
+  d = Descriptor(4,3,52,3)
   z = vars(d)
   k = params(d)
   map = track_ring([z[1], z[2], z[3], z[4]], 0.36+k[1], k[2], k[3:end])
@@ -94,9 +94,9 @@ function benchmark_ForwardDiff()
   m(z) = track_ring([z[1], z[2], z[3], z[4]], 0.36+z[5], z[6], z[7:end])
   j = Array{Float64}(undef,4,56)
   h = Array{Float64}(undef,224,56)
-  #c = Array{Float64}(undef,12544,56)
+  c = Array{Float64}(undef,12544,56)
   ForwardDiff.jacobian!(j, m, zeros(56))
   ForwardDiff.jacobian!(h, z->ForwardDiff.jacobian(z->m(z), z), zeros(56))
-  #ForwardDiff.jacobian!(c, z->ForwardDiff.jacobian(z->ForwardDiff.jacobian(z->m(z), z), z), zeros(56))
+  ForwardDiff.jacobian!(c, z->ForwardDiff.jacobian(z->ForwardDiff.jacobian(z->m(z), z), z), zeros(56))
   return j, h #, c
 end
